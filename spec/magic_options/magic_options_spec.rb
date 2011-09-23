416270d12050877ff4989cb7dc5f60b609098036
Sheldon Hearn <sheldonh@starjuice.net>
2011-09-23 21:32:16 +0200

    Improve rspec doc output
    * Be more specific about the module feature being described.
    * Prepare for specification of the initialize helper method.

--- spec/magic_options/magic_options_spec.rb
+++ spec/magic_options/magic_options_spec.rb
@@ -2,84 +2,106 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "MagicOptions" do
 
-  require 'magic_options'
-  class Klazz
-    include MagicOptions
-    attr_accessor :accessible
-  end
+  describe "#initialize" do
 
-  context "without specifying magic_options" do
+    context "with default magic_options" do
 
-    it "sets instance variables for all options" do
-      object = Klazz.new(:color => "green")
-      object.should be_instance_variable_defined(:@color)
-      object.instance_variable_get(:@color).should == "green"
-    end
+      before(:each) do
+        require 'magic_options'
+        class Klazz
+          include MagicOptions
+          attr_accessor :accessible
+        end
+      end
 
-  end
-
-  context "with magic_options :accessors" do
-
-    before(:each) do
-      Klazz.send :magic_options, :accessors
-    end
-
-    it "allows options named after accessors" do
-      lambda {
-        object = Klazz.new(:accessible => "is an accessor")
-      }.should_not raise_error
-    end
-
-    it "disallows any other option" do
-      lambda {
-        object = Klazz.new(:color => "is not an accessor")
-      }.should raise_error(ArgumentError)
-    end
-
-  end
-
-  context "with magic_options :color, :shape" do
-
-    before(:each) do
-      Klazz.send :magic_options, :color, :shape
-    end
-
-    it "allows an option called :color" do
-      lambda {
+      it "sets instance variables for all options" do
         object = Klazz.new(:color => "green")
-      }.should_not raise_error
+        object.should be_instance_variable_defined(:@color)
+        object.instance_variable_get(:@color).should == "green"
+      end
+
     end
 
-    it "allows options an option called :shape" do
-      lambda {
-        object = Klazz.new(:shape => "round")
-      }.should_not raise_error
+    context "with magic_options :accessors" do
+
+      before(:each) do
+        require 'magic_options'
+        class Klazz
+          include MagicOptions
+          magic_options :accessors
+          attr_accessor :accessible
+        end
+      end
+
+      it "allows options named after accessors" do
+        lambda {
+          object = Klazz.new(:accessible => "is an accessor")
+        }.should_not raise_error
+      end
+
+      it "disallows any other option" do
+        lambda {
+          object = Klazz.new(:color => "is not an accessor")
+        }.should raise_error(ArgumentError)
+      end
+
     end
 
-    it "disallows any other option" do
-      lambda {
-        object = Klazz.new(:size => "is not a magic option")
-      }.should raise_error
+    context "with magic_options :color, :shape" do
+
+      before(:each) do
+        require 'magic_options'
+        class Klazz
+          include MagicOptions
+          magic_options :color, :shape
+          attr_accessor :accessible
+        end
+      end
+
+      it "allows an option called :color" do
+        lambda {
+          object = Klazz.new(:color => "green")
+        }.should_not raise_error
+      end
+
+      it "allows options an option called :shape" do
+        lambda {
+          object = Klazz.new(:shape => "round")
+        }.should_not raise_error
+      end
+
+      it "disallows any other option" do
+        lambda {
+          object = Klazz.new(:size => "is not a magic option")
+        }.should raise_error
+      end
+
     end
 
-  end
+    context "given a disallowed option" do
 
-  context "with a disallowed option" do
+      before(:each) do
+        require 'magic_options'
+        class Klazz
+          include MagicOptions
+          magic_options :allowed
+          attr_accessor :accessible
+        end
+      end
 
-    before(:each) do
-      Klazz.send :magic_options, :allowed
-    end
 
-    it "raises an ArgumentError" do
-      lambda {
-        object = Klazz.new(:size => "is not a magic option")
-      }.should raise_error(ArgumentError)
-    end
+      it "raises an ArgumentError" do
+        lambda {
+          object = Klazz.new(:size => "is not a magic option")
+        }.should raise_error(ArgumentError)
+      end
 
-    it "says which class disallowed which option" do
-      lambda {
-        object = Klazz.new(:size => "is not a magic option")
-      }.should raise_error("Unknown option size in new Klazz")
+      it "says which class disallowed which option" do
+        lambda {
+          object = Klazz.new(:size => "is not a magic option")
+        }.should raise_error("Unknown option size in new Klazz")
+      end
+
     end
 
   end
