2e8645a351d6d6d1ce9f97f6acc65239d61da8da
Sheldon Hearn <sheldonh@starjuice.net>
2011-10-03 15:39:28 +0200

    Add strict mode and turn it off by default
    * Add new config option :strict, defaulted to false
    * When :strict is false, just ignore unexpected options.
    * Make the previous behaviour of throwing an ArgumentError available
      when :strict is true.
    
    This is unobtrusive ruby.

--- lib/magic_options.rb
+++ lib/magic_options.rb
@@ -42,21 +42,24 @@ module MagicOptions
   # The optional config hash may impose constraints on this behaviour as follows:
   #
   # [+:only+]
-  #   Specify the only options that are allowed.  The special value +:respond_to?+ specifies that options
-  #   are allowed if they name a method on the instance.
+  #   Specify the only options that may be assigned.  The special value +:respond_to?+ specifies that options
+  #   are assigned if they name a method on the instance.
   #
   # [+:require+]
   #   Specify options that must be passed.  Options specified in +:require+ do not also need to be
   #   specified in +:only+.
   #
+  # [+:strict+]
+  #   Specify that an +ArgumentError+ should be raised instead of ignoring unexpected options.  Defaults to false.
+  #
   # Raises:
   #
-  # [ArgumentError] when +:only+ is specified and an option is passed that is not in +:only+ or +:require+
+  # [ArgumentError] when +:strict+ is true, +:only+ is specified and an unexpected option is passed
   # [ArgumentError] when +:require+ specifies one or more options that are not passed
   def magic_options(options, config = {})
-    magic_options_validate(options, config)
+    magic_options_validate options, config
     options.each do |option, value|
-      instance_variable_set "@#{option}", value
+      instance_variable_set("@#{option}", value) if magic_options_wanted?(option, config)
     end
   end
 
@@ -103,21 +106,32 @@ module MagicOptions
 private
 
   def magic_options_validate(options, config) # :nodoc:
-    if config[:only] == :respond_to?
-      if unknown = options.keys.detect { |option| !respond_to?(option) }
-        raise ArgumentError, "Unknown option #{unknown} in new #{self.class}"
-      end
-    elsif config[:only]
-      only = [config[:only], config[:require]].flatten.compact
-      if unknown = options.keys.detect { |option| !only.include?(option) }
-        raise ArgumentError, "Unknown option #{unknown} in new #{self.class}"
-      end
+    if config[:strict]
+      options.keys.each do |option|
+        raise ArgumentError, "Unknown option #{option} in new #{self.class}" unless magic_options_wanted?(option, config)
+      end if config[:only] || config[:require]
     end
-    if (required = [config[:require]].flatten.compact).size > 0
-      if missing = required.detect { |requirement| !options.keys.include?(requirement) }
-        raise ArgumentError, "Missing option #{missing} in new #{self.class}"
-      end
+    if missing = magic_options_missing?(options, config)
+      raise ArgumentError, "Missing option #{missing} in new #{self.class}"
     end
+  end
+
+  def magic_options_missing?(options, config) # :nodoc:
+    if config[:require].is_a?(Enumerable)
+      return config[:require].detect { |required| !options.include?(required) }
+    elsif config[:require]
+      return config[:require] unless options.include?(config[:require])
+    end
+  end
+
+  def magic_options_wanted?(option, config) # :nodoc:
+    return true if !config[:only] && !config[:require]
+    return true if config[:only] == :respond_to? && respond_to?(option)
+    return true if config[:only].is_a?(Enumerable) && config[:only].include?(option)
+    return true if config[:only] == option
+    return true if config[:require].is_a?(Enumerable) && config[:require].include?(option)
+    return true if config[:require] == option
+    false
   end
 
 end
--- spec/magic_options/instance_method_spec.rb
+++ spec/magic_options/instance_method_spec.rb
@@ -10,87 +10,83 @@ describe "MagicOptions#magic_options(options, config = {})" do
     end
   end
 
-  it "sets instance variables named after keys in the options hash" do
+  it "assigns values from the options hash to instance variables named by the keys in the hash" do
     class Cow
       def initialize(options = {}); magic_options(options); end
     end
-    @cow = Cow.new(:name => 'Daisy', :color => :brown)
-    @cow.should be_instance_variable_defined('@name')
-    @cow.should be_instance_variable_defined('@color')
+    @cow = Cow.new(:name => 'Daisy')
+    @cow.instance_variable_get('@name').should == 'Daisy'
   end
 
-  it "sets instance variable values to the values in the options hash" do
+  it "assigns options named by config[:only] if config[:only] is given" do
     class Cow
-      def initialize(options = {}); magic_options(options); end
+      def initialize(options = {}); magic_options(options, :only => :name); end
     end
     @cow = Cow.new(:name => 'Daisy', :color => :brown)
     @cow.instance_variable_get('@name').should == 'Daisy'
-    @cow.instance_variable_get('@color').should == :brown
   end
 
-  it "accepts any option names without config[:only]" do
+  it "ignores options not named by config[:only] if config[:strict] is false" do
     class Cow
-      def initialize(options = {}); magic_options(options); end
+      def initialize(options = {}); magic_options(options, :only => :name); end
     end
-    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown, :gender => :female) }.should_not raise_error
+    @cow = Cow.new(:name => 'Daisy', :color => :brown)
+    @cow.should_not be_instance_variable_defined('@color')
   end
 
-  it "accepts option names included in config[:only]" do
+  it "raises an error for options not named by config[:only] if config[:strict] is true" do
     class Cow
-      def initialize(options = {}); magic_options(options, :only => [:name, :color]); end
-    end
-    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown) }.should_not raise_error
-  end
-
-  it "raises ArgumentError for option names not included in config[:only]" do
-    class Cow
-      def initialize(options = {}); magic_options(options, :only => [:name, :color]); end
+      def initialize(options = {}); magic_options(options, :only => [:name, :color], :strict => true); end
     end
     lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown, :gender => :female) }.should raise_error(ArgumentError)
   end
 
-  it "accepts the absence of options without config[:require]" do
+  it "assigns options named by instance methods if config[:only] is :respond_to?" do
+    class Cow
+      attr_accessor :name
+      def initialize(options = {}); magic_options(options, :only => :respond_to?); end
+    end
+    @cow = Cow.new(:name => 'Daisy')
+    @cow.instance_variable_get('@name').should == 'Daisy'
+  end
+
+  it "ignores options not named by instance methods if config[:only] is :respond_to? and config[:strict] is false" do
+    class Cow
+      attr_accessor :name
+      def initialize(options = {}); magic_options(options, :only => :respond_to?); end
+    end
+    @cow = Cow.new(:name => 'Daisy', :color => :brown)
+    @cow.should_not be_instance_variable_defined('@brown')
+  end
+
+  it "raises an error for options not named by instance methods if config[:only] is :respond_to? and config[:strict] is true" do
+    class Cow
+      attr_accessor :name
+      def initialize(options = {}); magic_options(options, :only => :respond_to?, :strict => true); end
+    end
+    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown) }.should raise_error(ArgumentError)
+  end
+
+  it "accepts an empty options hash without config[:require]" do
     class Cow
       def initialize(options = {}); magic_options(options); end
     end
     lambda { @cow = Cow.new }.should_not raise_error
   end
 
-  it "raises ArgumentError for absent option names in config[:require]" do
+  it "assigns options named by config[:require] as if named by config[:only] also" do
+    class Cow
+      def initialize(options = {}); magic_options(options, :require => :name, :only => :color); end
+    end
+    @cow = Cow.new(:name => 'Daisy', :color => :brown)
+    @cow.instance_variable_get('@name').should == 'Daisy'
+  end
+
+  it "raises an error for missing options named by config[:require]" do
     class Cow
       def initialize(options = {}); magic_options(options, :require => [:name, :color]); end
     end
     lambda { @cow = Cow.new(:name => 'Daisy') }.should raise_error(ArgumentError)
-  end
-
-  it "accepts option names in config[:require] even when they are not in config[:only]" do
-    class Cow
-      def initialize(options = {}); magic_options(options, :require => :name, :only => :color); end
-    end
-    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown) }.should_not raise_error
-  end
-
-  it "raises ArgumentError for option names absent from both config[:only] and config[:require]" do
-    class Cow
-      def initialize(options = {}); magic_options(options, :require => :name, :only => :color); end
-    end
-    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown, :gender => :female) }.should raise_error(ArgumentError)
-  end
-
-  it "accepts option names that match instance method names if config[:only] is :respond_to?" do
-    class Cow
-      attr_accessor :name, :color
-      def initialize(options = {}); magic_options(options, :only => :respond_to?); end
-    end
-    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown) }.should_not raise_error
-  end
-
-  it "raises ArgumentError for option names that match no instance method names if config[:only] is :respond_to?" do
-    class Cow
-      attr_accessor :name, :color
-      def initialize(options = {}); magic_options(options, :only => :respond_to?); end
-    end
-    lambda { @cow = Cow.new(:name => 'Daisy', :color => :brown, :gender => :female) }.should raise_error(ArgumentError)
   end
 
 end
